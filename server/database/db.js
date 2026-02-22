59b45949d28e108781f28454267152d507d823e3
Estevan Hernandes Silva de Oliveira <estevanhernandes1@gmail.com>
2026-02-22 19:10:35 -0300

    chore: configuração banco de dados SQLite

---
+++ server/database/db.js
@@ -0,0 +1,21 @@
+const sqlite = require("sqlite3").verbose();
+
+const db = new sqlite.Database("./database.sqlite", (err) => {
+  if (err) {
+    return console.error(err.message);
+  }
+
+  console.log("Connected to the database.");
+});
+
+db.run(`
+  CREATE TABLE IF NOT EXISTS usuarios (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome TEXT NOT NULL,
+    email TEXT NOT NULL UNIQUE,
+    status TEXT NOT NULL DEFAULT 'ativo'
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+  )
+`);
+
+module.exports = db;
